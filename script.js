80b9af8cdf681191c2a8960c34c124c64708f601
wakdj12 <awjonny29@gmail.com>
2023-04-05 11:29:31 +0100

    modal-design

--- script.js
+++ script.js
@@ -7,39 +7,6 @@ var btn = document.getElementsByClassName("myBtn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close");
 
-// When the user clicks the button, open the modal
-// btn[0].onclick = function () {
-//   modal[0].style.display = "block";
-// };
-
-// btn[1].onclick = function () {
-//   modal[1].style.display = "block";
-// };
-
-// btn[2].onclick = function () {
-//   modal[2].style.display = "block";
-// };
-
-// btn[3].onclick = function () {
-//   modal[3].style.display = "block";
-// };
-// // When the user clicks on <span> (x), close the modal
-// span[0].onclick = function () {
-//   modal[0].style.display = "none";
-// };
-
-// span[1].onclick = function () {
-//   modal[1].style.display = "none";
-// };
-
-// span[2].onclick = function () {
-//   modal[2].style.display = "none";
-// };
-
-// span[3].onclick = function () {
-//   modal[3].style.display = "none";
-// };
-
 for (let i = 0; i < 4; i++) {
   btn[i].onclick = function () {
     modal[i].style.display = "block";
