58bc5dedf1bbf4a0dcc98cbf1a6f47ae4dfb7158
wakdj12 <awjonny29@gmail.com>
2023-04-16 23:29:38 +0100

    decluttering

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var modal = document.getElementsByClassName("modal");
 
 // Get the button that opens the modal
-var btn = document.getElementsByClassName("myBtn");
+var btn = document.getElementsByClassName("btn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close");
@@ -15,16 +15,16 @@ for (let i = 0; i < 4; i++) {
   span[i].onclick = function () {
     modal[i].style.display = "none";
   };
+
+  window.onclick = function (event) {
+    if (event.target == modal[i]) {
+      modal[i].style.display = "none";
+    }
+    if (event.target == modal[i]) {
+      modal[i].style.display = "none";
+    }
+  };
 }
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal[0]) {
-    modal[0].style.display = "none";
-  }
-  if (event.target == modal[1]) {
-    modal[1].style.display = "none";
-  }
-};
 
 /*
  https://ipdata.co/blog/how-to-get-the-ip-address-in-javascript/
@@ -46,3 +46,36 @@ json(`https://api.ipdata.co/27.126.160.0/threat?api-key=${apiKey}`).then(
     console.log(data.blocklists);
   }
 );
+
+width = window.innerWidth;
+height = window.innerHeight;
+console.log(width, height);
+
+var x = document.querySelectorAll(".inline-d");
+
+x.forEach((e) => {
+  e.addEventListener("click", function () {
+    console.log(e.childNodes[1].className);
+    console.log(e.childNodes[1].textContent);
+    w = e.childNodes[1];
+
+    // document.querySelector(w).classList.toggle("expand");
+    s = getComputedStyle(w);
+    console.log();
+    if (s.display === "none") {
+      w.style.display = "inline";
+    } else if (s.display === "inline") {
+      w.style.display = "none";
+    }
+    //w.style.display = "none";
+
+    // var y = document.querySelectorAll(".defintion");
+    // y.forEach((z) => {
+    //   z.classList.toggle("expand");
+    // });
+  });
+});
+//});
+
+console.log(x);
+console.log();
