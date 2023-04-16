65f7422182b307ddf14e01ef92997fef5740596c
wakdj12 <awjonny29@gmail.com>
2023-04-16 20:08:56 +0100

    nuclear and infojs

--- script.js
+++ script.js
@@ -25,3 +25,24 @@ window.onclick = function (event) {
     modal[1].style.display = "none";
   }
 };
+
+/*
+ https://ipdata.co/blog/how-to-get-the-ip-address-in-javascript/
+*/
+function json(url) {
+  return fetch(url).then((res) => res.json());
+}
+
+let apiKey = "627c6d76f95b0ee0e7cef70893447a56f28f3859021e4cab28a183ac";
+json(`https://api.ipdata.co?api-key=${apiKey}`).then((data) => {
+  console.log(data.ip);
+  console.log(data.city);
+  console.log(data.country_code);
+  console.log(data.latitude, data.longitude);
+});
+
+json(`https://api.ipdata.co/27.126.160.0/threat?api-key=${apiKey}`).then(
+  (data) => {
+    console.log(data.blocklists);
+  }
+);
