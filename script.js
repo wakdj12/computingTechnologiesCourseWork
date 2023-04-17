a72e08e5dc3bae89377b2115f2ef4b8ad1afaa5c
wakdj12 <awjonny29@gmail.com>
2023-04-17 22:16:52 +0100

    17.04.23 End

--- script.js
+++ script.js
@@ -36,6 +36,7 @@ INFORMATION FROM API
 /*
  https://ipdata.co/blog/how-to-get-the-ip-address-in-javascript/
 */
+var pInfo = document.querySelector(".information");
 function json(url) {
   return fetch(url).then((res) => res.json());
 }
@@ -44,15 +45,15 @@ let apiKey = "627c6d76f95b0ee0e7cef70893447a56f28f3859021e4cab28a183ac";
 json(`https://api.ipdata.co?api-key=${apiKey}`).then((data) => {
   console.log(data.ip);
   console.log(data.city);
-  console.log(data.country_code);
   console.log(data.latitude, data.longitude);
+  pInfo.textContent = `IP Adress: ${data.ip}, City: ${data.city}, Approximate Latitude: ${data.latitude}, Approximate Longitude: ${data.longitude} `;
 });
 
-json(`https://api.ipdata.co/27.126.160.0/threat?api-key=${apiKey}`).then(
-  (data) => {
-    console.log(data.blocklists);
-  }
-);
+pInfo.textContent = json(
+  `https://api.ipdata.co/27.126.160.0/threat?api-key=${apiKey}`
+).then((data) => {
+  console.log(data.blocklists);
+});
 
 /*
 INFORMATION FROM API END
@@ -87,14 +88,19 @@ DECLUTTERING END
 
 /* calculating header height for internal link offset due to the sticky header */
 
-var title = document.querySelector(".shift");
+var title = document.querySelectorAll(".shift"),
+  i;
+console.log(title.length);
 var header = document.querySelector("header");
 
-var height = header.offsetHeight;
-console.log(height);
-var offsetSize = height;
-console.log(offsetSize);
-title.style.paddingTop = height + "px";
-title.style.paddingBottom = -height + "px";
+for (i = 0; i < title.length; i++) {
+  var height = header.offsetHeight;
+  // console.log(height);
+  var offsetSize = height;
+  console.log(offsetSize);
+  title[i].style.paddingTop = height + "px";
+  console.log(title[i]);
+  title[i].style.paddingBottom = -height + "px";
+}
 
 /* calculating header height for offset end */
